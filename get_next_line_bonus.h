9bcdb87ab9a02bd32134276d0f849bc61fce7db4
Anna Zinchenko <anna.s.zinchenko@gmail.com>
2024-06-28 16:25:29 +0300

    bonus header added

---
+++ get_next_line_bonus.h
@@ -0,0 +1,23 @@
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# ifndef OPEN_MAX
+#  define OPEN_MAX 1024
+# endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin_stash(char *stash, char *buf);
+char	*ft_substr(char *stash, unsigned int start, size_t len);
+size_t	find_nl(char *stash);
+
+#endif
